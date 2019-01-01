2f0a61e093bde1eaa52472fd7269f5ab96869f8b
miao shan <minnieshan@gmail.com>
2019-01-01 16:55:35 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,11 @@
-function forloop(array){
+function forLoop(array){
 for (var i=0; i<25; i++){
-  if(i==1){
-    "I am 1 strange loop";
+  if(i===1){
+  return  array.push("I am 1 strange loop.");
   }
-  else{"I am ${i} strange loops"}
+  else{
+    return array.push("I am ${i} strange loops.");
+  }
 }
-
+    return array;
 }
